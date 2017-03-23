084ec4a375077a72d27286f77dbc89e495ecd7cc
Slawek <yarpiin@gmail.com>
2017-03-23 19:47:18 +0000

    Update as for template version 3

--- config.sh
+++ config.sh
@@ -10,18 +10,12 @@
 # 
 # Instructions:
 # 
-# 1. Place your files into system folder
-# 2. Fill in all sections in this file
-# 3. For advanced features, add commands into the script files under common:
-#    post-fs.sh, post-fs-data.sh, service.sh
-# 4. Change the "module.prop" under common with the info of your module
-# 
-##########################################################################################
-##########################################################################################
-# 
-# Limitations:
-# 1. Can not place any new items under /system root!! e.g. /system/newfile, /system/newdir
-#    Magisk will delete these items at boot.
+# 1. Place your files into system folder (delete the placeholder file)
+# 2. Fill in your module's info into module.prop
+# 3. Configure the settings in this file (common/config.sh)
+# 4. For advanced features, add shell commands into the script files under common:
+#    post-fs-data.sh, service.sh
+# 5. For changing props, add your additional/modified props into common/system.prop
 # 
 ##########################################################################################
 
@@ -31,13 +25,16 @@
 
 # NOTE: This part has to be adjusted to fit your own needs
 
-# This will be the folder name under /magisk or /cache/magisk
+# This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
 MODID=ukm
 
-# Set to true if you need automount
+# Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
 AUTOMOUNT=true
+
+# Set to true if you need to load system.prop
+PROPFILE=false
 
 # Set to true if you need post-fs-data script
 POSTFSDATA=true
@@ -83,6 +80,7 @@ print_modname() {
 # By default Magisk will merge your files with the original system
 # Directories listed here however, will be directly mounted to the correspond directory in the system
 
+# You don't need to remove the example below, these values will be overwritten by your own list
 # This is an example
 REPLACE="
 /system/app/Youtube
@@ -91,7 +89,8 @@ REPLACE="
 /system/framework
 "
 
-# Construct your own list here
+# Construct your own list here, it will overwrite the example
+# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
 "
 
